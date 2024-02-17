1bbde026e54b04d8e6711d374e9b01c721e05d8b
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-02-17 18:35:13 +0900

    加速度、角速度、温度を送るプログラムを作成

--- M5Stick/Arduino/SendSensorInfo/SendSensorInfo.ino
+++ M5Stick/Arduino/SendSensorInfo/SendSensorInfo.ino
@@ -11,22 +11,20 @@ float gyroZ = 0;
 float temp = 0;
 
 void setup() {
-  Serial.begin(9600);
-
-  M5.Lcd.fillScreen(BLACK);
-
   M5.begin();
+  M5.Lcd.fillScreen(BLACK);
   M5.MPU6886.Init();
 }
 
 void loop() {
   M5.MPU6886.getAccelData(&accX, &accY, &accZ);
-  Serial.printf("%f, %f, %f", accX, accY, accZ);
+  Serial.printf("%.2f, %.2f, %.2f\n", accX * 100, accY * 100, accZ * 100);
 
   M5.MPU6886.getGyroData(&gyroX, &gyroY, &gyroZ);
-  Serial.printf("%f, %f, %f", gyroX, gyroY, gyroZ);
+  Serial.printf("%.2f, %.2f, %.2f\n", gyroX, gyroY, gyroZ);
+  
   M5.MPU6886.getTempData(&temp);
-  Serial.printf("%f", temp);
+  Serial.printf("%.2f C\n", temp);
 
   delay(100);
 }
